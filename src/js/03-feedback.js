868eb35ab9c20bc96804e4da4d0e980394d406db
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-08-07 13:41:59 +0100

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(onTextInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+getStorageData();
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+function onTextInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formEl.elements.email.value === '' || formEl.elements.message.value === '') {
+    alert('Enter something in field');
+  } else {
+    console.log(formData);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+
+    formData = {};
+  }
+}
+
+function getStorageData() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    formEl.elements.email.value = savedData.email || '';
+    formEl.elements.message.value = savedData.message || '';
+  }
+}
