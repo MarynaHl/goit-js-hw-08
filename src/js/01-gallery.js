b95ed8f400bc362aa1419918414a7a9b6448a5e6
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-08-07 13:38:52 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Add imports simplelightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+const markupGallery = createMarkupGallery(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', markupGallery);
+
+
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" style="display: block" />
+        </a>`;
+    })
+    .join('');
+}
+
+
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
